90e8544e0fb2ea53592560d3207526281fe41260
Arceus14 <arceus14regal@gmail.com>
2026-06-20 00:11:48 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, st: str) -> bool:
+        lst = list(st.replace(' ', ''))
+        for each in lst:
+            if not each.isalnum():
+                lst.remove(each)
+        
+        s = ''.join(lst)
+        l = len(s)
+        if not l%2 == 0:
+            fh = s[:l//2]
+            sh = s[l//2 + 1:]
+        else:
+            fh = s[: l//2]
+            sh = s[l//2 :]
+        if fh.lower() == sh.lower()[::-1]:
+            return True
+        else:
+            return False
