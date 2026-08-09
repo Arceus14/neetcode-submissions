01aae977cb2c5f76d8f29c59ea6dcaddaf5c8def
Arceus14 <arceus14regal@gmail.com>
2026-08-09 16:57:05 +0530

    Add: trapping-rain-water - submission-9

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-9.py
@@ -0,0 +1,25 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        size = len(height)
+
+        maxLeft = [0] * size
+        maxRight = [0] * size
+
+        rainArea = 0
+
+        ml = 0
+        for i, n in enumerate(height):
+            maxLeft[i] = ml
+            ml = max(ml, n)
+
+        mr = 0
+        for i, n in enumerate(reversed(height)):
+            maxRight[size - 1 - i] = mr
+            mr = max(mr, n)
+        
+        for i, n in enumerate(height):
+            area = min(maxLeft[i], maxRight[i]) - n
+            area = max(area, 0)
+            rainArea += area
+        return rainArea
+            
